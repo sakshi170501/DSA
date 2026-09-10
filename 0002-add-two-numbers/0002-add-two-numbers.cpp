262a53cd9d697937207cbaf75a2b9d19e90ededc
Sakshi Kumari <sakshikumaree44@gmail.com>
2026-09-10 17:28:44 +0530

    Time: 3 ms (34.32%), Space: 77.2 MB (12.87%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,38 +1,53 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        return recursive(l1,l2);
 
-        ListNode* res = NULL;
-        ListNode* curr = NULL;
-        int carry = 0;
+    }
+    //     ListNode* res = NULL;
+    //     ListNode* curr = NULL;
+    //     int carry = 0;
 
-        while (l1 != NULL || l2 != NULL || carry != 0) {
+    //     while (l1 != NULL || l2 != NULL || carry != 0) {
 
-            int sum = carry;
+    //         int sum = carry;
 
-            if (l1 != NULL) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
+    //         if (l1 != NULL) {
+    //             sum += l1->val;
+    //             l1 = l1->next;
+    //         }
 
-            if (l2 != NULL) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
+    //         if (l2 != NULL) {
+    //             sum += l2->val;
+    //             l2 = l2->next;
+    //         }
 
-            ListNode* newNode = new ListNode(sum % 10);
-            carry = sum / 10;
+    //         ListNode* newNode = new ListNode(sum % 10);
+    //         carry = sum / 10;
 
-            if (res == NULL) {
-                res = newNode;
-                curr = newNode;
-            }
-            else {
-                curr->next = newNode;
-                curr = curr->next;
-            }
+    //         if (res == NULL) {
+    //             res = newNode;
+    //             curr = newNode;
+    //         }
+    //         else {
+    //             curr->next = newNode;
+    //             curr = curr->next;
+    //         }
+    //     }
+
+    //     return res;
+    // }
+    ListNode* recursive(ListNode* l1,ListNode* l2,int carry=0){
+        if(!l1 && !l2 && !carry){
+            return nullptr;
         }
-
-        return res;
+        int a=l1?l1->val:0;
+        int b=l2?l2->val:0;
+        int sum=a+b+carry;
+        int digit=sum%10;
+        carry=sum/10;
+        ListNode* ans=new ListNode(digit);
+        ans->next=recursive(l1?l1->next:l1,l2?l2->next:l2,carry);
+        return ans;
     }
 };
